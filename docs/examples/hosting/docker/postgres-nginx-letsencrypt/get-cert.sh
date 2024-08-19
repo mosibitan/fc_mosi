59f108d5e7bff314ce2f66ed706f322b1c84f567
mosibitan <mosibitan@163.com>
2024-08-19 19:19:54 +0800

    Init deploy

--- docs/examples/hosting/docker/postgres-nginx-letsencrypt/get-cert.sh
+++ docs/examples/hosting/docker/postgres-nginx-letsencrypt/get-cert.sh
@@ -2,5 +2,5 @@ sudo docker run --rm --name certbot -p 80:80 \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     certbot/certbot certonly --standalone \
-    -d test.padloc.app \
-    --expand --non-interactive --agree-tos -m martin@padloc.app
+    -d mosi.link \
+    --expand --non-interactive --agree-tos -m mosibitan@163.com
